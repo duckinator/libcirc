55a9d9d4363bca1fb16ff288e3165dca67fd0640
Ellen Marie Dash <me@duckie.co>
2018-06-08 07:47:28 -0400

    Add old code I forgot to commit; not sure what state it's in.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,28 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+CIrcRecvCallback *circ_new(char *nick, char *server, CIrcRecvCallback *recv_callback)
+{
+    CIrcClient *client = malloc(sizeof(CIrcClient));
+    client->recv = recv_callback;
+
+    return client;
+}
+
+bool circ_close(CIrcClient *client)
+{
+    // TODO.
+    return false;
+}
+
+bool circ_register_recv_callback(CIrcClient *client,
+        CIrcRecvCallback *callback)
+{
+    circ_recv
+    return true;
+}
+
+
 int main()
 {
     printf("TODO.");
